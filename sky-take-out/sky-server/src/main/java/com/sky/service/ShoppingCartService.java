2526000afa344c9c371e7c64390ad8485ecb7aac
qiao2840078423 <106972396+qiao2840078423@users.noreply.github.com>
2025-04-26 18:50:33 +0800

    清空购物车业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartService {
 
@@ -10,4 +13,15 @@ public interface ShoppingCartService {
      * @return
      */
     Void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
+
+    /**
+     * 清空购物车
+     */
+    void cleanShoppingCart();
 }
