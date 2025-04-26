7457eb431df1701bf6575b21b57922e59059b127
qiao2840078423 <106972396+qiao2840078423@users.noreply.github.com>
2025-04-26 13:34:45 +0800

    缓存套餐代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -12,6 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
